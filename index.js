af4445ab624cebc5b94117aa4cb8f851ffaf95fd
Justin <justin.kjaroki@student.moringaschool.com>
2022-08-21 01:21:41 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting;
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else  if (feet > 2500){
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC" ? "Ok, sounds good." :"No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let responce;
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
